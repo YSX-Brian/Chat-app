25d3392dffe1634d649363ee6209c0591c8be9df
megidolaonn <101783502+megidolaonn@users.noreply.github.com>
2022-12-06 03:43:25 -0500

    added giftedchat

--- components/Chat.js
+++ components/Chat.js
@@ -1,21 +1,75 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Platform, KeyboardAvoidingView } from 'react-native';
+import { GiftedChat, Bubble } from 'react-native-gifted-chat'
+
 
 export default class Chat extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      messages: [],
+    }
+  }
+
   componentDidMount() {
     let name = this.props.route.params.name;
     this.props.navigation.setOptions({ title: name });
+    this.setState({
+      messages: [
+        {
+          _id: 1,
+          text: name + ' has entered the chatroom.',
+          createdAt: new Date(),
+          system: true,
+        },
+        {
+          _id: 2,
+          text: 'Hello developer!',
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        },
+      ]
+    })
+  }
+
+  onSend(messages = []) {
+    this.setState(previousState => ({
+      messages: GiftedChat.append(previousState.messages, messages),
+    }))
+  }
+
+  renderBubble(props) {
+    let background = this.props.route.params.background;
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          right: {
+            backgroundColor: '#000'
+          }
+        }}
+      />
+    )
   }
 
   render() {
     let background = this.props.route.params.background;
 
     return (
-      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: background }}>
-        {/* check if the background is black or purple and adjust text color */}
-        <Text style={{ color: background == '#090C08' || background == '#474056' ? 'white' : 'black' }}>
-          Welcome to the Chat Screen!
-        </Text>
+      <View style={{ flex: 1, backgroundColor: background }}>
+        <GiftedChat
+          renderBubble={this.renderBubble.bind(this)}
+          messages={this.state.messages}
+          onSend={messages => this.onSend(messages)}
+          user={{
+            _id: 1,
+          }}
+        />
+        {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
       </View>
     )
   }
